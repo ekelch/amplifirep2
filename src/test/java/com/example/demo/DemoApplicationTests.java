2de433ced957d389cb4d84f75f14e270664c0ca4
patchangeles <mariapatricia182@revature.net>
2022-09-21 02:02:50 -0400

    added configuration for testing

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,5 +1,6 @@
 package com.example.demo;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
 import java.util.List;
@@ -7,6 +8,7 @@ import java.util.List;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.amqp.RabbitProperties.Stream;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureWebMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
@@ -14,15 +16,20 @@ import com.revature.model.Users;
 import com.revature.repository.UserRepository;
 import com.revature.service.UserService;
 
-@SpringBootTest
+@SpringBootTest(classes=UserService.class)
 class DemoApplicationTests {
 
+	
 	@Autowired
 	private UserService service;
 
 	@MockBean
 	private UserRepository repository;
 
+	public void contextLoads() throws Exception{
+		assertThat(service).isNotNull();
+		assertThat(repository).isNotNull();
+	}
 	/*
 	 * @Test
 	 * 
@@ -34,7 +41,7 @@ class DemoApplicationTests {
 
 	@Test
 	public void registerTest() {
-		Users user = new Users(3, "user3", "pass3", "user3@email.com", "user testing", 99986);
+		Users user = new Users("user3", "pass3", "user3@email.com", "user testing", 99986);
 		when(repository.save(user)).thenReturn(user);
 		assertEquals(user, service.register(user));
 	}
