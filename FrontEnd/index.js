5cf3b9a97bf385ed31752b45d3ea98f240b6aa9f
Evan <evan.c.kelch@gmail.com>
2022-09-23 10:10:05 -0400

    modified js

--- FrontEnd/index.js
+++ FrontEnd/index.js
@@ -1,63 +1,79 @@
-let banner = document.querySelector("#banner");
+window.addEventListener("load", renderNav());
+// window.addEventListener("load", renderLogin());
 
-//window.addEventListener("load", renderLogin)
-function derenderPage(){
-    document.querySelector("body").innerHTML = "";
+function renderNav() {
+
+    let navigation = document.createElement("nav");
+    navigation.className = "navbar";
+    let navList = document.createElement("ul");
+    navList.className = "dawnnav";
+    let navUsers = document.createElement("LI");
+        navUsers.innerText = "Users";
+        navUsers.addEventListener("click", renderLogin);
+    let navRoutes = document.createElement("LI");
+        navRoutes.innerText = "Routes";
+        navRoutes.addEventListener("click", renderRoutes);
+    let navLocations = document.createElement("LI");
+        navLocations.innerText = "Locations";
+    let navSearch = document.createElement("LI");
+        navSearch.innerText = "Search";
+
+    navList.append(navUsers, navRoutes, navLocations, navSearch);
+
+    navigation.appendChild(navList);
+    document.querySelector("body").appendChild(navigation);
 }
+
 function renderLogin(){
+    derenderPage();
     let loginContainer = document.createElement("div");
     loginContainer.id = "login";
     loginContainer.className = "bannerLogin";
-
-    let usernameInput = document.createElement("input");
-
-    usernameInput.id = "username";
-    let usernameLabel = document.createElement("p");
+            
+    let usernameLabel = document.createElement("p");        // create username static label "Username"
     usernameLabel.innerText = "Username";
-
     loginContainer.appendChild(usernameLabel);
 
+    let usernameInput = document.createElement("input");    // create username input variable "user1"
+    usernameInput.id = "username";
     usernameInput.type = "text";
     usernameInput.placeholder = "username";
-
     loginContainer.appendChild(usernameInput);
     loginContainer.appendChild(document.createElement("br"));
 
-    let passwordLabel = document.createElement("p");
+    let passwordLabel = document.createElement("p");        // create password static label "Password"
     passwordLabel.innerText = "Password";
-
     loginContainer.appendChild(passwordLabel);
 
-    let passwordInput = document.createElement("input");
-
+    let passwordInput = document.createElement("input");    // create password input variable "pass1"
     passwordInput.type = "password";
     passwordInput.id = "password";
-
+    passwordInput.placeholder = "password";
     loginContainer.appendChild(passwordInput);
     loginContainer.appendChild(document.createElement("br"));
 
-    let submitButton = document.createElement("input");
-
+    let submitButton = document.createElement("input");     // create submit button, goes to asyncLogin
     submitButton.type = "button";
     submitButton.value = "submit";
-
     submitButton.addEventListener("click", asyncLogin);
-
     loginContainer.appendChild(submitButton);
 
-    let resetButton = document.createElement("input");
-
+    let resetButton = document.createElement("input");      // create reset button to clear input strings
     resetButton.type = "button";
     resetButton.value = "reset";
-
     resetButton.addEventListener("click", function(){
         usernameInput.value = "";
         passwordInput.value = "";
     });
-
     loginContainer.appendChild(resetButton);
 
-    document.querySelector("body").appendChild(loginContainer);
+    document.querySelector("body").appendChild(loginContainer); // adds login container to html body, below nav bar
+    
+}
+
+function derenderPage(){
+    document.querySelector("body").innerHTML = "";
+    renderNav();
 }
 
 function renderHomepage(data){
@@ -98,15 +114,7 @@ async function asyncLogin(){
     }
 }
   
-  
-  
-  
-  
-  
-  
-  
-  
-  
+ 
   var down = document.getElementById("login_DOWN");
   // Create a break line element
   var br = document.createElement("br");
@@ -118,40 +126,47 @@ function login_form() {
     form.setAttribute("method", "post");
     form.setAttribute("action", "#");
  
-        // Create an input element for username
-        var UN = document.createElement("input");
-        UN.setAttribute("type", "text");
-        UN.setAttribute("name", "username");
-        UN.setAttribute("placeholder", "User Name");
-    
-   
-    
-        // Create an input element for password
-        var PWD = document.createElement("input");
-        PWD.setAttribute("type", "password");
-        PWD.setAttribute("name", "password");
-        PWD.setAttribute("placeholder", "Password");
- 
-   
-                // create a submit button
-                var s = document.createElement("input");
-                s.setAttribute("type", "submit");
-                s.setAttribute("value", "Submit");
-                 
-                // Append the full name input to the form
-                form.appendChild(UN);
-                 
-                // Inserting a line break
-                form.appendChild(br.cloneNode());
-                 
-                // Append the Password to the form
-                form.appendChild(PWD);
-                form.appendChild(br.cloneNode());
-                 
+    // Create an input element for username
+    var UN = document.createElement("input");
+    UN.setAttribute("type", "text");
+    UN.setAttribute("name", "username");
+    UN.setAttribute("placeholder", "User Name");
+
+
+
+    // Create an input element for password
+    var PWD = document.createElement("input");
+    PWD.setAttribute("type", "password");
+    PWD.setAttribute("name", "password");
+    PWD.setAttribute("placeholder", "Password");
+
+
+    // create a submit button
+    var s = document.createElement("input");
+    s.setAttribute("type", "submit");
+    s.setAttribute("value", "Submit");
         
-                // Append the submit button to the form
-                form.appendChild(s);
- 
-                document.getElementsByTagName("body")[0]
-               .appendChild(form);
-            }
+    // Append the full name input to the form
+    form.appendChild(UN);
+        
+    // Inserting a line break
+    form.appendChild(br.cloneNode());
+        
+    // Append the Password to the form
+    form.appendChild(PWD);
+    form.appendChild(br.cloneNode());
+        
+
+    // Append the submit button to the form
+    form.appendChild(s);
+
+    document.getElementsByTagName("body")[0]
+    .appendChild(form);
+}
+
+function renderRoutes() {
+    derenderPage();
+    let routeBanner = document.createElement("h1");
+    routeBanner.innerText = "Quinsana Plus";
+    document.querySelector("body").append(routeBanner);
+}
